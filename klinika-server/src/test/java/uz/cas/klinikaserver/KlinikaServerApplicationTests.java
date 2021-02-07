c8ac7171e2916bf4b5cd829dbaef651ac3eabcee
user <nodirbekmamadaliyev1998@gmail.com>
2021-02-07 14:09:42 +0500

    klinikani hosil qilish

---
+++ klinika-server/src/test/java/uz/cas/klinikaserver/KlinikaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.cas.klinikaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KlinikaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
